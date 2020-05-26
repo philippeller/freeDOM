fbfb9a8dd24c1eab6b017fba649121e1b3df3b5a
Philipp Eller <peller.phys@gmail.com>
2020-05-26 11:41:11 +0200

    interleaved datasets with block shuffling for drastically reduced memoery footprint

--- freedom/utils/dataset.py
+++ freedom/utils/dataset.py
@@ -18,7 +18,7 @@ class Data():
         self.labels = labels
         self.geo = geo
 
-    def get_hitnet_data(self, train_batch_size=1024, test_batch_size=256, test_size=0.01, random_state=42):
+    def get_hitnet_data(self, train_batch_size=1024, test_batch_size=256, shuffle_block_size=2**14, test_size=0.01, random_state=42):
 
         data = []
         for dir in self.dirs:
@@ -31,13 +31,13 @@ class Data():
                                                                             test_size=test_size,
                                                                             random_state=random_state)
         
-        train = self.get_dataset(hits_train, params_train, batch_size=train_batch_size)
-        test = self.get_dataset(hits_test, params_test, batch_size=test_batch_size, test=True)
+        train = self.get_dataset(hits_train, params_train, batch_size=train_batch_size, shuffle_block_size=shuffle_block_size)
+        test = self.get_dataset(hits_test, params_test, batch_size=test_batch_size, test=True, shuffle_block_size=shuffle_block_size)
         
         return train, test
         
         
-    def get_chargenet_data(self, train_batch_size=1024, test_batch_size=256, test_size=0.01, random_state=42):
+    def get_chargenet_data(self, train_batch_size=1024, test_batch_size=256, shuffle_block_size=2**14, test_size=0.01, random_state=42):
 
         data = []
         for dir in self.dirs:
@@ -50,12 +50,12 @@ class Data():
                                                                                 test_size=test_size,
                                                                                 random_state=random_state)
         
-        train = self.get_dataset(charge_train, params_train, batch_size=train_batch_size)
-        test = self.get_dataset(charge_test, params_test, batch_size=test_batch_size, test=True)
+        train = self.get_dataset(charge_train, params_train, batch_size=train_batch_size, shuffle_block_size=shuffle_block_size)
+        test = self.get_dataset(charge_test, params_test, batch_size=test_batch_size, test=True, shuffle_block_size=shuffle_block_size)
         
         return train, test
         
-    def get_stringnet_data(self, train_batch_size=1024, test_batch_size=256, test_size=0.01, random_state=42):
+    def get_stringnet_data(self, train_batch_size=1024, test_batch_size=256, shuffle_block_size=2**14, test_size=0.01, random_state=42):
 
         data = []
         for dir in self.dirs:
@@ -68,13 +68,13 @@ class Data():
                                                                                 test_size=test_size,
                                                                                 random_state=random_state)
         
-        train = self.get_dataset(string_train, params_train, batch_size=train_batch_size)
-        test = self.get_dataset(string_test, params_test, batch_size=test_batch_size, test=True)
+        train = self.get_dataset(string_train, params_train, batch_size=train_batch_size, shuffle_block_size=shuffle_block_size)
+        test = self.get_dataset(string_test, params_test, batch_size=test_batch_size, test=True, shuffle_block_size=shuffle_block_size)
         
         return train, test
         
         
-    def get_dataset(self, x, p, batch_size=1024, test=False):
+    def get_dataset(self, x, t, shuffle_block_size=2**14, batch_size=1024, test=False):
         '''
         get a tensorflow dataset for likelihood approximation
 
@@ -83,8 +83,10 @@ class Data():
         -----------
         x : ndarray
             observations
-        p : ndarray
+        t : ndarray
             parameters        
+        shuffle_block_size : int
+            block size over which to shuffle, should be multiple of batch_size
         batch_size : int
         test : bool
             no shuffling, prefetching and caching
@@ -93,34 +95,53 @@ class Data():
         --------
         
         tf.data.Dataset
-            with structure ((x, p), y) for training
+            with structure ((x, t), y) for training
         
         '''
         
         N = x.shape[0]
-        assert p.shape[0] == N
+        assert t.shape[0] == N
         
         d_x = tf.data.Dataset.from_tensor_slices(x)
-        d_p = tf.data.Dataset.from_tensor_slices(p)
+        d_t = tf.data.Dataset.from_tensor_slices(t)
 
         d_true_labels = tf.data.Dataset.from_tensor_slices(np.ones((N, 1), dtype=x.dtype))
         d_false_labels = tf.data.Dataset.from_tensor_slices(np.zeros((N, 1), dtype=x.dtype))
 
-        d_X = tf.data.Dataset.concatenate(d_x, d_x)
-        d_P = tf.data.Dataset.concatenate(d_p, d_p.shuffle(N))
+        d_xs = tf.data.Dataset.from_tensor_slices([d_x, d_x]).interleave(lambda x : x)
+        d_ts = tf.data.Dataset.from_tensor_slices([d_t, d_t.shuffle(shuffle_block_size)]).interleave(lambda x : x)
+        d_ys = tf.data.Dataset.from_tensor_slices([d_true_labels, d_false_labels]).interleave(lambda x : x)
         
-        d_inputs = tf.data.Dataset.zip((d_X, d_P))        
-        d_outputs = tf.data.Dataset.concatenate(d_true_labels, d_false_labels)
+        
+        dataset = tf.data.Dataset.zip((tf.data.Dataset.zip((d_xs, d_ts)), d_ys))
+  
+        return dataset.batch(batch_size)
+        
+        # batch before shuffle
+        
+#         d_x_windows = d_x.window(shuffle_block_size)
+#         d_t_windows = d_t.window(shuffle_block_size)
+#         d_true_labels_windows = d_true_labels.window(shuffle_block_size)
+#         d_false_labels_windows = d_false_labels.window(shuffle_block_size)
+        
+        
+#         # shuffled, i.e. p(x)p(t) vs p(x,t)
+        
+#         d_X = tf.data.Dataset.concatenate(d_x_windows, d_x_windows)
+#         d_T = tf.data.Dataset.concatenate(d_t_windows, d_t_windows.map(lambda x: x.shuffle(shuffle_block_size)))
+        
+#         d_inputs = tf.data.Dataset.zip((d_X, d_T))        
+#         d_outputs = tf.data.Dataset.concatenate(d_true_labels_windows, d_false_labels_windows)
 
 
-        dataset = tf.data.Dataset.zip((d_inputs, d_outputs))
+#         dataset = tf.data.Dataset.zip((d_inputs, d_outputs))
         
-        if test:
-            return dataset.batch(batch_size)
+#         if test:
+#             return dataset.batch(batch_size)
         
         
-        batched = dataset.shuffle(2*N).batch(batch_size=batch_size)
+#         batched = dataset.shuffle(2*N).batch(batch_size=batch_size)
         
-        prefetched = batched.prefetch(1).cache()
+#         prefetched = batched.prefetch(1)
         
-        return prefetched
+#         return prefetched
