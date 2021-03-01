8b100402b985ee28a78e84c38f89a483e1111671
Aaron Fienberg <atfienberg@psu.edu>
2021-03-01 14:41:01 -0500

    create I3Particles from fit result

--- freedom/reco/i3freedom.py
+++ freedom/reco/i3freedom.py
@@ -90,47 +90,51 @@ class I3FreeDOMClient:
 
         delta = time.time() - start
         fit_res["delta"] = delta
-        print(f"Recoing this event took {delta*1000:.1f} ms")
 
-        self.write_output_data(frame, suffix, fit_res)
+        store_reco_output(frame, suffix, fit_res)
 
-    @staticmethod
-    def write_output_data(frame, suffix, fit_res):
-        """add reco output to an i3frame"""
-        from icecube.dataclasses import I3VectorString, I3VectorDouble, I3Double
-        from icecube.icetray import I3Int, I3Bool
 
-        prefix = f"FreeDOM_{suffix}_"
+def store_reco_output(frame, suffix, fit_res):
+    """store reco output in an i3frame"""
+    from icecube.dataclasses import I3VectorString, I3VectorDouble, I3Double
+    from icecube.icetray import I3Int, I3Bool
 
-        par_names = i3frame_dataloader.DEFAULT_LABELS
-        frame[f"{prefix}par_names"] = to_i3_vec(par_names, I3VectorString)
+    prefix = f"FreeDOM_{suffix}_"
 
-        frame[f"{prefix}best_fit"] = to_i3_vec(fit_res["x"], I3VectorDouble)
-        frame[f"{prefix}success"] = I3Bool(fit_res["success"])
+    par_names = i3frame_dataloader.DEFAULT_LABELS
+    frame[f"{prefix}par_names"] = to_i3_vec(par_names, I3VectorString)
 
-        for double_p, double_frame_name in [("fun", "best_LLH"), ("delta", "delta_T")]:
-            frame[f"{prefix}{double_frame_name}"] = I3Double(float(fit_res[double_p]))
+    frame[f"{prefix}best_fit"] = to_i3_vec(fit_res["x"], I3VectorDouble)
+    frame[f"{prefix}success"] = I3Bool(fit_res["success"])
 
-        try:
-            frame[f"{prefix}truth_LLH"] = I3Double(float(fit_res["truth_LLH"]))
-        except KeyError:
-            pass
+    for double_p, double_frame_name in [("fun", "best_LLH"), ("delta", "delta_T")]:
+        frame[f"{prefix}{double_frame_name}"] = I3Double(float(fit_res[double_p]))
 
-        for int_p, int_frame_name in [
-            ("n_calls", "n_llh_calls"),
-            ("nit", "n_crs_iters"),
-            ("stopping_flag", "stopping_flag"),
-        ]:
-            frame[f"{prefix}{int_frame_name}"] = I3Int(fit_res[int_p])
+    try:
+        frame[f"{prefix}truth_LLH"] = I3Double(float(fit_res["truth_LLH"]))
+    except KeyError:
+        pass
 
-        postfit_res = fit_res.get("postfit", {})
+    for int_p, int_frame_name in [
+        ("n_calls", "n_llh_calls"),
+        ("nit", "n_crs_iters"),
+        ("stopping_flag", "stopping_flag"),
+    ]:
+        frame[f"{prefix}{int_frame_name}"] = I3Int(fit_res[int_p])
 
-        for key, val in postfit_res.items():
-            if key == "envs":
-                for i, env_ps in enumerate(zip(*val)):
-                    frame[f"{prefix}env_p{i}"] = to_i3_vec(env_ps, I3VectorDouble)
-            else:
-                frame[f"{prefix}{key}"] = to_i3_vec(val, I3VectorDouble)
+    postfit_res = fit_res.get("postfit", {})
+
+    for key, val in postfit_res.items():
+        if key == "envs":
+            for i, env_ps in enumerate(zip(*val)):
+                frame[f"{prefix}env_p{i}"] = to_i3_vec(env_ps, I3VectorDouble)
+        else:
+            frame[f"{prefix}{key}"] = to_i3_vec(val, I3VectorDouble)
+
+    reco_pars = {name: val for name, val in zip(par_names, fit_res["x"])}
+    reco_pars["success"] = fit_res["success"]
+    for particle_type in ("neutrino", "cascade", "track"):
+        frame[f"{prefix}{particle_type}"] = build_i3_particle(reco_pars, particle_type)
 
 
 def to_i3_vec(array, i3_vec_type):
@@ -139,3 +143,40 @@ def to_i3_vec(array, i3_vec_type):
     for val in array:
         i3_vec.append(val)
     return i3_vec
+
+
+_energy_getters = dict(
+    cascade=lambda pars: pars["cascade_energy"],
+    track=lambda pars: pars["track_energy"],
+    neutrino=lambda pars: pars["track_energy"] + pars["cascade_energy"],
+)
+
+
+def build_i3_particle(reco_pars, particle_type):
+    """build an I3Particle from reco parameters"""
+    from icecube.dataclasses import I3Particle, I3Constants, I3Position, I3Direction
+    from icecube.icetray import I3Units
+
+    shape_map = dict(
+        cascade=I3Particle.ParticleShape.Cascade,
+        track=I3Particle.ParticleShape.ContainedTrack,
+        neutrino=I3Particle.ParticleShape.Primary,
+    )
+
+    particle = I3Particle()
+
+    if reco_pars["success"]:
+        particle.fit_status = I3Particle.FitStatus.OK
+    else:
+        particle.fit_status = I3Particle.GeneralFailure
+
+    particle.dir = I3Direction(reco_pars["zenith"], reco_pars["azimuth"])
+    particle.energy = _energy_getters[particle_type](reco_pars) * I3Units.GeV
+    particle.length = np.nan
+    particle.pdg_encoding = I3Particle.ParticleType.unknown
+    particle.pos = I3Position(*[reco_pars[d] * I3Units.m for d in ("x", "y", "z")])
+    particle.shape = shape_map[particle_type]
+    particle.time = reco_pars["time"] * I3Units.ns
+    particle.speed = I3Constants.c
+
+    return particle
